cfeab8f058742ccdfad166fd7a2c91daf9104a58
Laurence Elsdon <git@elsdon.me>
2026-03-20 15:12:54 +0000

    feat: Add `studiole-di` and `studiole-di-macros` crates

---
+++ crates/di/src/service_provider.rs
@@ -0,0 +1,128 @@
+use crate::prelude::*;
+
+/// Container for registering and resolving services.
+pub struct ServiceProvider {
+    factories:
+        HashMap<TypeId, Box<dyn Fn(&ServiceProvider) -> Arc<dyn Any + Send + Sync> + Send + Sync>>,
+    instances: StdMutex<HashMap<TypeId, Arc<dyn Any + Send + Sync>>>,
+}
+
+impl ServiceProvider {
+    /// Create an empty [`ServiceProvider`].
+    #[must_use]
+    pub fn new() -> Self {
+        Self {
+            factories: HashMap::new(),
+            instances: StdMutex::new(HashMap::new()),
+        }
+    }
+
+    /// Register a pre-built instance of a service.
+    #[allow(clippy::as_conversions)]
+    #[must_use]
+    pub fn with_instance<T>(self, instance: T) -> Self
+    where
+        T: Service + 'static,
+    {
+        let type_id = TypeId::of::<T>();
+        let dynamic = Arc::new(instance) as Arc<dyn Any + Send + Sync>;
+        self.instances
+            .lock()
+            .expect("should be able to lock instances")
+            .insert(type_id, dynamic);
+        self
+    }
+
+    /// Register a factory function for creating a service on demand.
+    #[allow(clippy::as_conversions)]
+    #[must_use]
+    pub fn with_factory<T, F>(mut self, factory: F) -> Self
+    where
+        T: Service + 'static,
+        F: Fn(&ServiceProvider) -> T + Send + Sync + 'static,
+    {
+        self.factories.insert(
+            TypeId::of::<T>(),
+            Box::new(move |services| Arc::new(factory(services)) as Arc<dyn Any + Send + Sync>),
+        );
+        self
+    }
+
+    /// Resolve a service by type.
+    ///
+    /// - Returns an existing instance if one was registered or previously created
+    /// - Calls a registered factory if available
+    /// - Falls back to [`Service::from_services`] to construct the service
+    pub async fn get_service<T: Service>(&self) -> Result<Arc<T>, Report<ServiceError>> {
+        let type_name = type_name::<T>();
+        trace!(type = type_name, "Resolving service");
+        let type_id = TypeId::of::<T>();
+        let option = self
+            .get_existing_service::<T>(type_id, type_name)
+            .or_else(|| self.create_registered_service::<T>(type_id, type_name));
+        if let Some(service) = option {
+            return Ok(service);
+        }
+        self.create_unregistered_service::<T>(type_id, type_name)
+            .await
+    }
+
+    #[allow(clippy::panic)]
+    fn get_existing_service<T: Service>(&self, type_id: TypeId, type_name: &str) -> Option<Arc<T>> {
+        let instances = self
+            .instances
+            .lock()
+            .expect("should be able to lock instances");
+        let dynamic = instances.get(&type_id)?;
+        let instance = Arc::clone(dynamic)
+            .downcast::<T>()
+            .unwrap_or_else(|_| panic!("should be able to downcast to {type_name}"));
+        Some(instance)
+    }
+
+    #[allow(clippy::panic)]
+    fn create_registered_service<T: Service>(
+        &self,
+        type_id: TypeId,
+        type_name: &str,
+    ) -> Option<Arc<T>> {
+        let factory = self.factories.get(&type_id)?;
+        let dynamic = factory(self);
+        let instance = Arc::clone(&dynamic)
+            .downcast::<T>()
+            .unwrap_or_else(|_| panic!("should be able to downcast to {type_name}"));
+        self.instances
+            .lock()
+            .expect("should be able to lock instances")
+            .insert(type_id, dynamic);
+        Some(instance)
+    }
+
+    #[allow(clippy::as_conversions)]
+    async fn create_unregistered_service<T: Service>(
+        &self,
+        type_id: TypeId,
+        type_name: &str,
+    ) -> Result<Arc<T>, Report<ServiceError>> {
+        let instance = T::from_services(self)
+            .await
+            .change_context(ServiceError::Create)
+            .attach_with("type", || String::from(type_name))?;
+        let instance = Arc::new(instance);
+        let dynamic = Arc::clone(&instance) as Arc<dyn Any + Send + Sync>;
+        self.instances
+            .lock()
+            .expect("should be able to lock instances")
+            .insert(type_id, dynamic);
+        Ok(instance)
+    }
+}
+
+/// Errors returned by [`ServiceProvider`].
+#[derive(Debug, Error)]
+pub enum ServiceError {
+    #[error("Unable to resolve service")]
+    NoService,
+    #[error("Unable to create service")]
+    Create,
+}
